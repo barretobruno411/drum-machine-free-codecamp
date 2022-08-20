07731980cd077ec0a785b977ba434faf3b3e2d55
Bruno Duarte <bruno.duarte411@gmail.com>
2022-08-20 10:01:46 -0300

    solved problem on App.js

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <h1>Drum Machine</h1>
       <Display />
       <footer>
-        <h2>Code By: <a href="https://github.com/barretobruno411" target="_blank">barretobruno411</a></h2>
+        <h2>Code By: <a href="https://github.com/barretobruno411" target="_blank" rel="noreferrer">barretobruno411</a></h2>
       </footer>
     </div>
   );
